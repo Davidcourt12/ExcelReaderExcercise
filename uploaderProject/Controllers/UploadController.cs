0ea0fe5a2fde2ed0e1e4fc50ce4596e7e1f7a9a6
David Court <david@turfsport.co.za>
2020-07-27 13:39:27 +0200

    Add project files.

---
+++ uploaderProject/Controllers/UploadController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace uploaderProject.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class UploadController : Controller
+    {
+        private IHostingEnvironment _hostingEnvironment;
+
+        public UploadController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpPost, DisableRequestSizeLimit]
+        public ActionResult UploadFile()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                string folderName = "Upload";
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                string newPath = Path.Combine(webRootPath, folderName);
+                if (!Directory.Exists(newPath))
+                {
+                    Directory.CreateDirectory(newPath);
+                }
+                if (file.Length > 0)
+                {
+                    string fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    string fullPath = Path.Combine(newPath, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                        InterpretExcel(file, fullPath);
+                    }
+
+                    
+                }
+                return Json("Upload Successful.");
+            }
+            catch (Exception ex)
+            {
+                return Json("Upload Failed: " + ex.Message);
+            }
+        }
+
+        public void InterpretExcel(IFormFile file, string fullPath)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            FileInfo fi = new FileInfo(fullPath);
+            using (ExcelPackage excelPackage = new ExcelPackage(fi))
+            {
+                
+                //Get a WorkSheet by name. If the worksheet doesn't exist, throw an exeption
+                ExcelWorksheet namedWorksheet = excelPackage.Workbook.Worksheets["MM Lotto"];
+
+                var lottoNames = new List<string>();
+
+                for (int i = 1; i < 100; i++)
+                {
+                    var cellText = namedWorksheet.Cells[$"A{i}"].ToText().Replace(",", "");
+                    if (cellText != null && cellText != "")
+                    { lottoNames.Add(cellText); }
+                    
+                }
+
+                //Save your file
+                //excelPackage.Save();
+            }
+        }
+
+    }
+}
